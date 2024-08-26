bc1c17c9cb105455748b9abe9911797f63240267
Eugene <evgenii.evgenii.02@mail.ru>
2024-08-26 18:32:06 +0300

    переделывание кода под адекватный счетчик категорий и продуктов

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 class Product:
-    """ Информация о свойтвах продуктах"""
-    name: str  # название продукта
-    description: str  # описание  продукта
-    price: str  # цена  продукта
-    quantity: float  # количество продукта
+    """Продукт"""
+
+    name: str
+    description: str
+    price: float
+    quantity: int
 
     def __init__(self, name, description, price, quantity):
         self.name = name
@@ -13,22 +14,21 @@ class Product:
 
 
 class Category:
-    """ Информация о котегориях """
+    """Категория товара"""
 
     category_count = 0
     product_count = 0
 
-    name: str  # название продукта
-    description: str  # описание продукта
-    products: str  # список товаров категории
+    name: str
+    description: str
+    products: list
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
         self.products = products
-
         Category.category_count += 1
-        Category.product_count += 1
+        Category.product_count += len(products)
 
 
 if __name__ == "__main__":
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,30 +1,97 @@
 import pytest
 
-from src.main import Product, Category
+from src.main import Category, Product
 
 
 @pytest.fixture
-def samsung():
-    return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+def first_product():
+    return Product(
+        name="Product",
+        description="Description of the product",
+        price=84.50,
+        quantity=10,
+    )
 
 
 @pytest.fixture
-def category():
-    return Category("Смартфоны",
-                    "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для "
-                    "удобства жизни",
-                    1)
+def second_product():
+    return Product(
+        name="Product number two",
+        description="Description of the product number two",
+        price=155.87,
+        quantity=34,
+    )
 
 
-def test_product(samsung):  # тест на продукт
-    assert samsung.name == "Samsung Galaxy S23 Ultra"
-    assert samsung.description == "256GB, Серый цвет, 200MP камера"
-    assert samsung.price == 180000.0
-    assert samsung.quantity == 5
+@pytest.fixture
+def first_category():
+    return Category(
+        name="Category",
+        description="Description of the category",
+        products=[
+            Product(
+                name="Product",
+                description="Description of the product",
+                price=84.50,
+                quantity=10,
+            ),
+            Product(
+                name="Product number two",
+                description="Description of the product number two",
+                price=155.87,
+                quantity=34,
+            ),
+        ],
+    )
 
 
-def test_category(category):
-    assert category.name == "Смартфоны"
-    assert category.description == ("Смартфоны, как средство не только коммуникации, но и получения дополнительных "
-                                    "функций для удобства жизни")
-    assert category.products == 1
+@pytest.fixture
+def second_category():
+    return Category(
+        name="Category number two",
+        description="Description of the category number two",
+        products=[
+            Product(
+                name="Product",
+                description="Description of the product",
+                price=84.50,
+                quantity=10,
+            ),
+            Product(
+                name="Product number two",
+                description="Description of the product number two",
+                price=155.87,
+                quantity=34,
+            ),
+            Product(
+                name="Product three",
+                description="Description of the product three",
+                price=8467.56,
+                quantity=32,
+            ),
+        ],
+    )
+
+
+def test_product(first_product, second_product):
+    assert first_product.name == "Product"
+    assert first_product.description == "Description of the product"
+    assert first_product.price == 84.50
+    assert first_product.quantity == 10
+
+    assert second_product.name == "Product number two"
+    assert second_product.description == "Description of the product number two"
+    assert second_product.price == 155.87
+    assert second_product.quantity == 34
+
+
+def test_category(first_category, second_category):
+    assert first_category.name == "Category"
+    assert first_category.description == "Description of the category"
+    assert len(first_category.products) == 2
+
+    assert first_category.category_count == 2
+    assert second_category.category_count == 2
+
+    assert first_category.product_count == 5
+    assert second_category.product_count == 5
